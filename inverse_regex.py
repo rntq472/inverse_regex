ec91490dcf73463e56cb23757fe1f4cbb7a309af
Jasper Watson <44103694+rntq472@users.noreply.github.com>
2021-10-26 16:03:02 +0200

    Added support for numpy and pandas

--- inverse_regex.py
+++ inverse_regex.py
@@ -7,7 +7,7 @@ from functools import singledispatch
 
 @singledispatch
 def inverse_regex(x, **args):
-    raise TypeError()
+    raise TypeError('Type not supported')
 
 @inverse_regex.register(bool)
 def _(x, **args):
@@ -29,19 +29,54 @@ def _(x, **args):
 def _(x, **args):
     return(tuple(inverse_regex(list(x), **args)))
 
+## If the user doesn't have numpy installed they don't need methods for those types.
 try:
-    import pandas as pd
-    @inverse_regex.register(pd.DataFrame)
+    import numpy as np
+    
+    @inverse_regex.register(np.bool_)
     def _(x, **args):
-        pass ## TODO
+        return(x)
+    
+    @inverse_regex.register(np.int_)
+    @inverse_regex.register(np.intc)
+    @inverse_regex.register(np.intp)
+    @inverse_regex.register(np.int8)
+    @inverse_regex.register(np.int16)
+    @inverse_regex.register(np.int32)
+    @inverse_regex.register(np.int64)
+    @inverse_regex.register(np.uint8)
+    @inverse_regex.register(np.uint16)
+    @inverse_regex.register(np.uint32)
+    @inverse_regex.register(np.uint64)
+    @inverse_regex.register(np.float_)
+    @inverse_regex.register(np.float16)
+    @inverse_regex.register(np.float32)
+    @inverse_regex.register(np.float64)
+    def _(x, **args):
+        return(inverse_regex(str(x), **args))
+    
+    @inverse_regex.register(np.ndarray)
+    def _(x, **args):
+        if x.ndim <= 2:
+            return(np.array([inverse_regex(y, **args) for y in x]))
+        else:
+            raise AttributeError('No more than two dimensions accepted')
 except:
     pass
 
+## If the user doesn't have pandas installed they don't need methods for those types.
 try:
-    import numpy as np
-    @inverse_regex.register(np.ndarray)
+    import pandas as pd
+    
+    @inverse_regex.register(pd.Series)
     def _(x, **args):
-        pass ## TODO
+        return(pd.Series([inverse_regex(y, **args) for y in x], index = x.index))
+    
+    @inverse_regex.register(pd.DataFrame)
+    def _(x, **args):
+        out = pd.DataFrame(x.apply(inverse_regex, axis = 1, **args))
+        out.columns = x.columns
+        return(out)
 except:
     pass
 
